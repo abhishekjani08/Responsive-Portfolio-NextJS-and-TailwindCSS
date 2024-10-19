78a602b40957f8a468fd47450fb1b5cf85b90558
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2024-10-19 23:11:06 +0530

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,47 +1,36 @@
-"use client" // this is a client component
-import React from "react"
-import Image from "next/image"
-import { Link } from "react-scroll/modules"
-import { HiArrowDown } from "react-icons/hi"
-import { useTheme } from "next-themes"
-import "./HeroSection.css"
+"use client"; // this is a client component
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
+import { useTheme } from "next-themes";
+import "./HeroSection.css";
 
 import {
   AiOutlineGithub,
-  AiOutlineTwitter,
   AiOutlineLinkedin,
-  AiOutlineYoutube,
-  AiOutlineBehance,
-  AiOutlineInstagram,
   AiOutlineLink,
+  AiOutlineBehance,
   AiOutlineWhatsApp,
 } from "react-icons/ai";
 
 const HeroSection = () => {
-  // const [currentTheme, setCurrentTheme] = useState('dark');
-  // const toggleTheme = () => {
-  //   setCurrentTheme(currentTheme === 'dark' ? 'light' : 'dark');
-  // };
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
-  const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme === "system" ? systemTheme : theme
+  // Set the theme on initial mount
+  useEffect(() => {
+    if (theme === "system") {
+      setTheme(systemTheme);
+    }
+  }, [systemTheme, setTheme, theme]);
 
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-8 sm:py-32 md:py-32 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-          {/* <Image
-            src={currentTheme === 'dark' ? '/AJ.png' : '/jani6.jpg'}
-            alt=""
-            width={300}
-            height={300}
-            className="headerimg"
-          /> */}
-
           {currentTheme === "light" ? (
-            <button
-              onClick={() => setTheme("dark")}
-            >
+            <button onClick={() => setTheme("dark")}>
               <Image
                 src="/AJ2.png"
                 alt=""
@@ -51,10 +40,7 @@ const HeroSection = () => {
               />
             </button>
           ) : (
-            <button
-              onClick={() => setTheme("light")}
-
-            >
+            <button onClick={() => setTheme("light")}>
               <Image
                 src="/AJ.png"
                 alt=""
@@ -67,25 +53,23 @@ const HeroSection = () => {
         </div>
         <div className="md:mt-2 md:w-3/5">
           <div className="fontsfamily">
-
-            <h1 className="text-4xl  font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m
+            <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+              Hi, I&#39;m
               <div className="text-teal-500">Abhishek Jani
                 <div className="waving-hand">&nbsp;
                   <div className="hand">
                     <span role="img" aria-label="Waving Hand" className="hand-emoji">
                       <Image src="/icons8-waving-hand-803.png" alt=""
                         width={50}
-                        height={50}></Image></span>
+                        height={50}></Image>
+                    </span>
                   </div>
                 </div>
-
               </div>
             </h1>
             <div className="text-md mt-4 mb-6 md:text-2xl">
               <p className="animated-text-1 ">I&#39;m a{" "}
-                <span className="font-medium text-teal-500">
-                  Software Developer  {" "}
-                </span> at <span className="font-medium text-teal-500">ARCON</span>
+                <span className="font-medium text-teal-500">Software Developer  {" "}</span> at <span className="font-medium text-teal-500">ARCON</span>
               </p>
 
               <p id="animated-text-2">MERN & MEAN Stack Developer</p>
@@ -147,21 +131,19 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center">
-      <Link
-        to="about"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}
-      >
-        <>
+        <Link
+          to="about"
+          activeClass="active"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
           <HiArrowDown size={35} className="animate-bounce" />
-        </>
-      </Link>
-    </div>
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
